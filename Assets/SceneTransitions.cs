7cad46c03207a9027b6f4e46743b8a935db1b8eb
Lena Loustan <lululeica@yandex.com>
2020-10-21 17:45:28 +0200

    Lenka

---
+++ Assets/SceneTransitions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour {
+
+    public Animator transitionAnim;
+    public string SceneName;
+
+    void update() {
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            StartCoroutine(Loadscene());
+
+        }
+
+    }
+    IEnumerator Loadscene(){
+
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(SceneName);
+    }
+}
+
+
