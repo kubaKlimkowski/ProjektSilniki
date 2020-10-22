ba7963dd845f10b2fd7e6eb3a7a461379d4c57fa
kubaKlimkowski <kubaklim7@gmail.com>
2020-10-22 13:11:43 +0200

    Raycast i wskaźnik

---
+++ Assets/skrypty/RayCastInteraction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CastRay();
+        }
+    }
+
+    void CastRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+
+        if (hit)
+        {
+            if(hit.transform.name == "Item")
+            {
+                Debug.Log("mam to");
+            }
+
+            if(hit.transform.name == "Item2")
+            {
+                Debug.Log("mam też to");
+            }
+        }
+    }
+}
