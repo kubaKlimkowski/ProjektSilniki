ac71616faeed124744e8dc47630c176f9a89d564
unknown <>
2020-10-27 11:51:28 +0100

    nowy system EQ

---
+++ Assets/EqController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EqController : MonoBehaviour {
+
+    public static EqController Instance { get; private set; }
+
+    public int firstFree = 0;
+    EqSlot[] slots;
+
+    private void Awake() {
+        Instance = this;
+        slots = GetComponentsInChildren<EqSlot>();
+    }
+
+
+
+    public void PutInEq(GameObject go) {
+        go.transform.position = slots[firstFree].transform.position;
+        slots[firstFree].isTaken = true;
+        firstFree++;
+    }
+}
---
+++ Assets/EqSlot.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EqSlot : MonoBehaviour {
+    public bool isTaken;
+}
