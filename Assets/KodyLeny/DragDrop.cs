bd4fe44624eb1a57045ad47eb5a9edb1e2265d69
Lena Loustan <lululeica@yandex.com>
2020-11-10 13:58:49 +0100

    poprawne

---
+++ Assets/KodyLeny/DragDrop.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IDragHandler, IBeginDragHandler, IEndDragHandler {
+    
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+       
+    }
+   
+    public void OnBeginDrag(PointerEventData eventData) {
+        canvasGroup.alpha = .5f;
+        canvasGroup.blocksRaycasts = false;
+
+
+    }
+
+    public void OnDrag(PointerEventData eventData){
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+
+    }
+
+
+
+}
