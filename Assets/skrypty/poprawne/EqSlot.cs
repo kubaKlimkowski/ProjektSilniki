5fa3470e61ed28d5a80788ab865b4276c840f03a
unknown <>
2020-11-17 12:08:33 +0100

    poprawiona wersja EQ

--- Assets/skrypty/poprawne/EqSlot.cs
+++ Assets/skrypty/poprawne/EqSlot.cs
@@ -8,8 +8,21 @@ public class EqSlot : MonoBehaviour {
     public bool isTaken;
     public GameObject heldObject;
 
+    private void Awake() {
+        isTaken = false;
+    }
     public void Free() {
         isTaken = false;
+
+        //heldObject.transform.SetParent(null); // czy ma być w canvasie czy nie(jeżeli w canvasie, można parenta ustawić jako root transform.SetParent(heldObject.transform.root);
+
         heldObject = null;
+    }
+    public void Take(GameObject go) {
+
+        isTaken = true;
+        heldObject = go;
+        heldObject.transform.SetParent(this.transform);
+        heldObject.transform.position = transform.position;
     }
 }
--- Assets/skrypty/poprawne/nowedzialajace/DragSystem.cs
+++ Assets/skrypty/poprawne/nowedzialajace/DragSystem.cs
@@ -14,58 +14,60 @@ public class DragSystem : MonoBehaviour  {
 
   
     public void StartDrag(GameObject pickedObject) {
-        RaycastHit2D[] hitObject = Physics2D.RaycastAll(Input.mousePosition, Vector3.forward);
-        Debug.DrawRay(Input.mousePosition, Vector3.forward, Color.green, 60);
-        MyItem hitObj = hitObject[0].collider.gameObject.GetComponent<MyItem>();
+        //RaycastHit2D[] hitObject = Physics2D.RaycastAll(Input.mousePosition, Vector3.forward);
+        //Debug.DrawRay(Input.mousePosition, Vector3.forward, Color.green, 60);
+        //MyItem hitObj = hitObject[0].collider.gameObject.GetComponent<MyItem>();
 
-        if(hitObj == null)
-            Debug.Log("nie trafiony obiekt");
+        //if(hitObj == null)
+        //    Debug.Log("nie trafiony obiekt");
 
 
-        if(hitObj.isInEq) {
-            for(int i = 0; i < hitObject.Length; i++) {
-                if(hitObject[i].collider.gameObject == pickedObject) continue;
-            }
-        }
+        //if(hitObj.isInEq) {
+        //    for(int i = 0; i < hitObject.Length; i++) {
+        //        if(hitObject[i].collider.gameObject == pickedObject) continue;
+        //    }
+        //}
 
-        currentDragObject = pickedObject;
+        //currentDragObject = pickedObject;
 
-        if(!currentDragObject.transform.parent.name.Equals("Canvas")) {
-            currentDragParent = currentDragObject.transform.parent.gameObject;
-            currentDragObject.transform.SetParent(mainParent);
-        }
+        //if(!currentDragObject.transform.parent.name.Equals("Canvas")) {
+        //    currentDragParent = currentDragObject.transform.parent.gameObject;
+        //    currentDragObject.transform.SetParent(mainParent);
+        //}
     }
 
     public void DragObject() {
-       currentDragObject.transform.position = Input.mousePosition;
+       //currentDragObject.transform.position = Input.mousePosition;
     }
 
     public void StopDrag() {
-        RaycastHit2D[] hitObject = Physics2D.RaycastAll(Input.mousePosition, Vector3.forward);
-        // Debug.DrawRay(Input.mousePosition, Vector3.forward, Color.green, 60);
-        if(isOutOfEq(hitObject)) {
-            for(int i = 0; i < hitObject.Length; i++) {
-                if(hitObject[i].collider.gameObject == currentDragObject) continue;
+        //RaycastHit2D[] hitObject = Physics2D.RaycastAll(Input.mousePosition, Vector3.forward);
+        //// Debug.DrawRay(Input.mousePosition, Vector3.forward, Color.green, 60);
+        //if(isOutOfEq(hitObject)) {
+        //    for(int i = 0; i < hitObject.Length; i++) {
+        //        if(hitObject[i].collider.gameObject == currentDragObject) continue;
 
-                if(hitObject[i].collider.gameObject.GetComponent<RequireObjectType>() != null) {
-                    if(hitObject[i].collider.gameObject.GetComponent<RequireObjectType>().requireObjectType == currentDragObject.GetComponent<RequireSlotType>().requireSlotType) {
-                        ModifyObject(hitObject[i]);
-                    } else {
-                        currentDragObject.transform.SetParent(currentDragParent.transform);
-                        EqSystem.instance.SetSlotOccupied(hitObject[i].collider.gameObject);
-                    }
-                } else {
-                    ModifyObject(hitObject[i]);
-                }
-            }
-        } else {
-            currentDragObject.transform.SetParent(currentDragParent.transform);
-            EqSystem.instance.SetSlotOccupied(currentDragParent);
-        }
-        currentDragObject = null;
-        currentDragParent = null;
+        //        RequireObjectType obj = hitObject[i].collider.gameObject.GetComponent<RequireObjectType>();
 
-        EqSystem.instance.SetSlotFree(hitObject[0].collider.gameObject);
+        //        if(obj != null) {
+        //            if(obj.requireObjectType == currentDragObject.GetComponent<RequireSlotType>().requireSlotType) {
+        //                ModifyObject(hitObject[i]);
+        //            } else {
+        //                currentDragObject.transform.SetParent(currentDragParent.transform);
+        //                EqSystem.instance.SetSlotOccupied(obj.gameObject);
+        //            }
+        //        } else {
+        //            ModifyObject(hitObject[i]);
+        //        }
+        //    }
+        //} else {
+        //    currentDragObject.transform.SetParent(currentDragParent.transform);
+        //    EqSystem.instance.SetSlotOccupied(currentDragParent);
+        //}
+        //currentDragObject = null;
+        //currentDragParent = null;
+
+        //EqSystem.instance.SetSlotFree(hitObject[0].collider.gameObject);
     }
 
     void ModifyObject(RaycastHit2D hitObject)
