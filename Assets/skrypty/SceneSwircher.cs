1c0310d9464de59dd175f749f8af0d8dcca10dda
GerryPL <Shedow59@gmail.com>
2020-11-23 13:45:34 +0100

    SceneSwitcher

---
+++ Assets/skrypty/SceneSwircher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwircher : MonoBehaviour
+{
+   
+    public void Switch()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void SwitchBack()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
