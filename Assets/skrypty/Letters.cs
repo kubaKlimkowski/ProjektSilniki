e04e22fe10318ab1c625ef549c3b3532a919e446
kubaKlimkowski <kubaklim7@gmail.com>
2020-12-14 21:25:16 +0100

    Oprawa Graficzna

---
+++ Assets/skrypty/Letters.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letters : MonoBehaviour
+{
+    public GameObject list;
+
+    public Transform worek;
+
+    public void Connection()
+    {
+        list.transform.SetParent(worek);
+
+        
+    }
+}
