d0119d21b8a4c6f7aee6b037c03d1f46337c8490
unknown <>
2021-01-14 13:56:37 +0100

    Transition system

--- Assets/Narzędzie/Tool.cs
+++ Assets/Narzędzie/Tool.cs
@@ -2,52 +2,48 @@
 using UnityEngine;
 
 
-public class Tool : EditorWindow
-{
-    //public Texture2D cpu;
-   // string myString = "Name of Canvas";
+public class Tool : EditorWindow {
+
+    public Texture2D tex;
+    // string myString = "Name of Canvas";
 
     int _selected = 0;
-    string[] _rooms = new string[5] { "Office","MailRoom","Kitchen","Computer","Coffe" };
-    
+
+    string[] _rooms = new string[5] { "Office", "MailRoom", "Kitchen", "Computer", "Coffe" };
+
 
     [MenuItem("Window/CanvasSwitching")]
-   
-    public static void ShowWindow()
-    {
+
+    public static void ShowWindow() {
         GetWindow<Tool>("CanvasSwitching");
 
     }
 
-    
 
 
-    private void OnGUI()
-    {
+
+    private void OnGUI() {
         GUILayout.Label("Here you can see where you will go from the canvas.", EditorStyles.boldLabel);
 
         EditorGUI.BeginChangeCheck();
 
         this._selected = EditorGUILayout.Popup("Canvas", _selected, _rooms);
-
-        if (EditorGUI.EndChangeCheck())
-        {
-            Debug.Log(_rooms[_selected]);  
+        tex = (Texture2D)EditorGUILayout.ObjectField(tex, typeof(Texture2D));
+        if(EditorGUI.EndChangeCheck()) {
+            Debug.Log(_rooms[_selected]);
         }
-        
 
-       
+
+        GUI.DrawTexture(EditorGUILayout.GetControlRect(), tex, ScaleMode.ScaleAndCrop, true, 10.0F);
         //GUI.Label(new Rect(15, 15, cpu.width, cpu.height), cpu);
-        
+
         //myString = EditorGUILayout.TextField("Canvas",myString);
 
-        if (GUILayout.Button("Check"))
-        {
+        if(GUILayout.Button("Check")) {
             check();
         }
-}
-    void check()
-    {
+    }
+    void check() {
         Debug.Log("dymy");
     }
 
---
+++ Assets/TransitionButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TransitionButton : MonoBehaviour {
+
+    [SerializeField]
+    public string level;
+
+    void Start() {
+        GetComponent<Button>().onClick.AddListener(Switch);
+    }
+
+    private void Switch() {
+        GetComponentInParent<TransitionController>().ChangeLevel(level);
+    }
+
+}
